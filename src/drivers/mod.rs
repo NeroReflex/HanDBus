c304271d4d1556577f7e4ddb5f371262d88a77cf
Denis Benato <benato.denis96@gmail.com>
2024-03-20 03:23:00 +0100

    WIP: dualsense support

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,2 +1,3 @@
 pub mod lego;
 pub mod steam_deck;
+pub mod dualsense;
--- src/input/target/mod.rs
+++ src/input/target/mod.rs
@@ -6,6 +6,7 @@ pub mod keyboard;
 pub mod mouse;
 pub mod steam_deck;
 pub mod xb360;
+pub mod dualsense;
 
 /// A [TargetDevice] is any virtual input device that emits input events
 #[derive(Debug)]
