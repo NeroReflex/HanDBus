50e2db2096d163c45186a510b522f56f4d79f5a8
Denis Benato <benato.denis96@gmail.com>
2024-12-28 17:34:46 +0100

    temp

--- src/input/source/led/multicolor_chassis.rs
+++ src/input/source/led/multicolor_chassis.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, fmt::Debug};
 
-use crate::{input::source::SourceOutputDevice, udev::device::UdevDevice};
+use crate::{input::source::{SourceInputDevice, SourceOutputDevice}, udev::device::UdevDevice};
 
 /// OrangePi Neo Touchpad source device implementation
 pub struct MultiColorChassis {
@@ -12,6 +12,16 @@ impl MultiColorChassis {
     /// device information
     pub fn new(device_info: UdevDevice) -> Result<Self, Box<dyn Error + Send + Sync>> {
         Ok(Self { device_info })
+    }
+}
+
+impl SourceInputDevice for MultiColorChassis {
+    fn poll(&mut self) -> Result<Vec<crate::input::event::native::NativeEvent>, crate::input::source::InputError> {
+        Ok(Vec::new())
+    }
+
+    fn get_capabilities(&self) -> Result<Vec<crate::input::capability::Capability>, crate::input::source::InputError> {
+        Ok(Vec::new())
     }
 }
 
